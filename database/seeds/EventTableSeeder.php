05cc81962afafa40302835591ca39e94d547fdc1
Lucas Miiller <lucasmiillerbtc@gmail.com>
2019-12-01 11:50:34 -0300

    Melhora predição de seeders

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -14,45 +14,42 @@ class EventTableSeeder extends Seeder
         $events = [];
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.874507 + $x/  1000,
-                'lng' =>  -43.246532 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.874507 + $x/300,
+                'lng' =>  -43.246532 + $x/300]);
         }
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.736131 + $x/  1000,
-                'lng' =>  -43.307953 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.736131 + $x/300,
+                'lng' =>  -43.307953 - $x/300]);
         }
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.876111 + $x/  1000,
-                'lng' =>  -43.246453 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.876111 - $x/300,
+                'lng' =>  -43.246453 + $x/300]);
         }
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.878475 + $x/  1000,
-                'lng' =>  -43.249061 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.878475 - $x/300,
+                'lng' =>  -43.249061 - $x/300]);
         }
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.880081 + $x/  1000,
-                'lng' =>  -43.246872 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.880081 + $x/300,
+                'lng' =>  -43.246872 + $x/300]);
         }
 
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.880565 + $x/  1000,
-                'lng' =>  -43.244374 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.880565 + $x/300,
+                'lng' =>  -43.244374 + $x/300]);
         }
 
 
 
         for ($x = 1; $x <= 50; $x++) {
-            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.880565 + $x/  1000,
-                'lng' =>  -43.244374 + $x/1000]);
+            array_push($events,  [ 'type_event_id' => 1, 'lat' => -22.880565 + $x/300,
+                'lng' =>  -43.244374 + $x/300]);
         }
-
-
-
 
         DB::table('events')->insert($events);
     }
